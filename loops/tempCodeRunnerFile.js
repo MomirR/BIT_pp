6ed2bf5832b4ae54d5512d5145b5c1df84e14bb2
BIT Student <classroom@bgit.rs>
2019-05-07 18:02:27 +0200

    loops,functions

--- loops/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-var niz = [4, 2, 2, 1, -6];
-for (i = 0; i < niz.length; i++) {
-        if (niz[i] < 0) {
-            console.log(niz[i]);            
-        }
-}
---
+++ loops/vezbanjeNaCasu.js
@@ -0,0 +1,40 @@
+//practicing 1.
+
+// var array = [
+//     ["bit", "tib"],
+//     ["it", "ti"],
+//     ["bt", "tb", "bb"]
+// ];
+// var res = "";
+// for (i = 0; i < array.length; i++) {
+//     var niz = array[i];
+
+//     for (j = 0; j < niz.length; j++) {
+//         res += niz[j];
+//     }
+//     res += "\n";
+
+// }
+// console.log(res);
+
+//practicing 2.
+
+// ***
+// * *
+// ***
+//
+// var stars = [
+//     ["*", "*", "*"],
+//     ["*", " ", "*"],
+//     ["*", "*", "*"]
+// ];
+// function printingStars() {
+
+
+//     for (i = 0; i < stars.length; i++) {
+//         var row = "";
+//         for (j = 0; j < row.length; j++)
+//     }
+
+// }
+// var res = printingStars();
