05abafe153799c3d7f70b12f05729738cd205c1c
Speedara <momirradovanovic@yahoo.com>
2019-05-11 20:11:21 +0200

    vezbeee

--- Week3/fun_practical_6/fun7.js
+++ Week3/fun_practical_6/fun7.js
@@ -1,16 +1,17 @@
 // Write a function to find the maximum element in array of numbers. 
 // Filter out all non-number elements.
-var testingArray = [3, 5, 24, "Hello", "World", undefined, NaN, 983];
+var allDataTypes = [3, 5, 24, "Hello", "World", undefined, NaN, 983];
 
-function filterNonNumber(testingArray) {
+function filterNonNumber(allDataTypes) {
     var numbers = [];
-    var testingArrayLength = testingArray.length;
-    for (i = 0, j = 0; i < testingArrayLength; i++) {
-        if (typeof testingArray[i] && isNaN(testingArray[i])) {
-            numbers[j] = testingArray[i];
+    var allDataTypesLength = allDataTypes.length;
+    for (i = 0, j = 0; i < allDataTypesLength; i++) {
+        if (typeof allDataTypes[i] === "number" && isFinite(allDataTypes[i])) {
+            numbers[j] = allDataTypes[i];
             j++;
         }
-
     }
-
+    return numbers;
 }
+
+console.log(filterNonNumber(allDataTypes));
