e3642663f5e19eaecf3dd4d3236f070d4e6a9f9b
BIT Student <classroom@bgit.rs>
2019-05-29 16:27:27 +0200

    Movie-form 2.0

--- Week6/Movie-form/js/app.js
+++ Week6/Movie-form/js/app.js
@@ -1,1 +1,31 @@
-console.log("Cao ja sam app");
+//Path
+var $titleInput = document.querySelector("#title");
+var $lengthInput = document.querySelector("#length");
+var $dropInput = document.querySelector("#drop-down");
+var $mainButton = document.querySelector("#button");
+var $errorPar = document.querySelector("#error");
+var $printingPar = document.querySelector("#printing");
+
+function mainFunction(e) {
+    e.preventDefault();
+    var titleValue = $titleInput.value.trim();
+    var lengthValue = $lengthInput.value.trim();
+    var dropValue = $dropInput.value.trim();
+
+    var firstMovie = new Movie(titleValue, lengthValue, dropValue);
+    var finalRes = firstMovie.getData();
+
+    if (titleValue === "" || lengthValue === "" || dropValue === "") {
+        $errorPar.textContent = "Doslo je do greske. Molimo vas popunite sva polja!";
+    }
+
+    else {
+        // printingPar.innerHTML = "<p id='printing'>" + finalRes + "</p>";
+        var newEle = document.createElement("p");
+        newEle.textContent = finalRes;
+        $printingPar.appendChild(newEle);
+        $errorPar.textContent = "";
+    }
+}
+
+$mainButton.addEventListener("click", mainFunction);
--- Week6/Movie-form/js/entities.js
+++ Week6/Movie-form/js/entities.js
@@ -1,1 +1,23 @@
+class Movie {
+    constructor(name, duration, genre) {
+        this.name = name;
+        this.duration = duration;
+        this.genre = genre;
+    }
+
+    formatedString() {
+        var string = "";
+        var up = this.genre.toUpperCase();
+        string += up[0];
+        string += up[up.length - 1];
+
+        return string;
+    }
+
+    getData() {
+        return this.name + " ,  " + this.duration + " ,  " + this.formatedString();
+    }
+}
+
+
 
